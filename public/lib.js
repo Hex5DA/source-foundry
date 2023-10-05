a3900b1ec35bf7eae1938d6ce45f8ed2de72a005
0x5DA <>
2023-10-05 22:01:36 +0100

    too lazy for a commit message

---
+++ public/lib.js
@@ -0,0 +1,21 @@
+// <https://stackoverflow.com/a/69122877>
+function getFormattedTime(time) {
+    const date = time instanceof Date ? time : new Date(time);
+    const fmt = new Intl.RelativeTimeFormat("en");
+    const ranges = {
+        years: 1 * 60 * 60 * 24 * 365,
+        months: 1 * 60 * 60 * 24 * 30,
+        weeks: 1 * 60 * 60 * 24 * 7,
+        days: 1 * 60 * 60 * 24,
+        hours: 1 * 60 * 60,
+        minutes: 1 * 60,
+        seconds: 1,
+    };
+
+    const elapsed = (date.getTime() - Date.now()) / 1000;
+    for (const [range, interval] of Object.entries(ranges)) {
+        if (interval < Math.abs(elapsed)) {
+            return fmt.format(Math.round(elapsed / interval), range);
+        }
+    }
+}
