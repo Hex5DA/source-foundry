ff2669a7061fca76ec0d4a0c60e463beec2390b6
Hex5DA <wasandland@outlook.com>
2024-01-06 18:56:04 +0100

    js stuff working now

--- public/falcon.js
+++ public/falcon.js
@@ -255,6 +255,16 @@ _registerEvents(EventTarget);
 // _registerEvents($State);
 
 HTMLElement.prototype.$select = function(option) {
+    for (child of this.children) {
+        if (child.value != option)
+            child.remove();
+    }
+
+    if (!this.children.length)
+        throw "`option` was listed";
+};
+
+HTMLElement.prototype.$reselect = function(option) {
     let found = false;
     for (child of this.children) {
         child.style.display = child.value === option ? "block" : "none";
